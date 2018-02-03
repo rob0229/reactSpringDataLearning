9de382c00eec888dd0e84236bff4c0c860158124
Rob0229 <rob0229@gmail.com>
2018-02-03 13:31:38 -0500

    removed target and added config

--- src/main/java/com/rob0229/reactSpringDataLearning/src/main/java/controller/HomeController.java
+++ src/main/java/com/rob0229/reactSpringDataLearning/src/main/java/controller/HomeController.java
@@ -1,13 +1,15 @@
 package com.rob0229.reactSpringDataLearning.src.main.java.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 	
 	@RequestMapping(value = "/")
-	public String index() {
+	public String home(Model model) {
+		 model.addAttribute("pageTitle","Welcome to my Awesome Dynamic Application");
 		return "index";
 	}
 	
